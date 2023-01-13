eaa9b2113f083b498651e1fe5da3ac65253b60b5
jjanmo <jjanmo07@hanmail.net>
2023-01-13 22:25:58 +0900

    refactor: seperate file of making csv logic

--- app.py
+++ app.py
@@ -1,25 +1,20 @@
 from flask import Flask
-
 from scrapper.wwr import extract_wwr_job
 from scrapper.indeed import extract_indeed_job
+from file import save_to_file
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def home():
     keyword = input("What do you want to search for?")
 
     wwr_jobs = extract_wwr_job(keyword)
     indeed_jobs = extract_indeed_job(keyword)
     jobs = wwr_jobs + indeed_jobs
 
-    file = open(f'{keyword}.csv', 'w')
-    file.write('Position, Company, Location, Link \n')
-
-    for job in jobs:
-        file.write(f'{job["position"]}, {job["company"]}, {job["location"]}, {job["link"]} \n')
-    file.close()
+    save_to_file(keyword, jobs)
 
     return 'hello world'
 
